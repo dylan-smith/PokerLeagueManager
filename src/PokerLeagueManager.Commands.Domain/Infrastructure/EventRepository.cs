ec1cf8a42a24d3c4edcd5665e6e61c589931169c
Dylan Smith <dylansmith256@hotmail.com>
2012-09-25 00:50:56 +0000

    Implemented GetAggregateById ***NO_CI***
    work-items: #32

--- src/PokerLeagueManager.Commands.Domain/Infrastructure/EventRepository.cs
+++ src/PokerLeagueManager.Commands.Domain/Infrastructure/EventRepository.cs
@@ -3,11 +3,13 @@ using PokerLeagueManager.Common.Events.Infrastructure;
 using PokerLeagueManager.Common.Utilities;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.IO;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace PokerLeagueManager.Commands.Domain.Infrastructure
 {
@@ -60,7 +62,30 @@ namespace PokerLeagueManager.Commands.Domain.Infrastructure
 
         public T GetAggregateById<T>(Guid aggregateId) where T : IAggregateRoot
         {
-            throw new NotImplementedException();
+            if (aggregateId == Guid.Empty)
+            {
+                throw new ArgumentException(string.Format("Invalid Aggregate ID ({0})", aggregateId.ToString()));
+            }
+
+            T result = System.Activator.CreateInstance<T>();
+
+            // TODO: Need to introduce snapshot functionality
+            var allEvents = _databaseLayer.GetDataTable(
+                "SELECT EventData, EventType FROM Events WHERE AggregateId = @AggregateId ORDER BY EventTimestamp",
+                "@AggregateId", aggregateId.ToString());
+
+            if (allEvents.Rows.Count == 0)
+            {
+                throw new ArgumentException(string.Format("No Aggregate with the specified ID was found ({0})", aggregateId.ToString()));
+            }
+
+            foreach (DataRow row in allEvents.Rows)
+            {
+                var e = CreateEventFromDataRow(row);
+                result.ApplyEvent(e);
+            }
+
+            return (T)result;
         }
 
         private string SerializeEvent<T>(T e)
@@ -76,6 +101,17 @@ namespace PokerLeagueManager.Commands.Domain.Infrastructure
 
                 return reader.ReadToEnd();
             }
+        }
+
+        private IEvent CreateEventFromDataRow(DataRow row)
+        {
+            StringReader reader = new StringReader((string)row["EventData"]);
+            XmlReader xr = XmlReader.Create(reader);
+            
+            Type eventType = Type.GetType((string)row["EventType"], true);
+
+            DataContractSerializer ser = new DataContractSerializer(eventType);
+            return (IEvent)ser.ReadObject(xr);
         }
     }
 }
