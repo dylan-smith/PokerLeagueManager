df3b04a985f833b55c2c76e57501bf8ee16a25fe
Dylan Smith <dylansmith256@hotmail.com>
2012-10-28 06:31:30 +0000

    Added a test for a Zero Placing ***NO_CI***
    work-items: #74

---
+++ src/PokerLeagueManager.Commands.Tests/EnterGameResults/ZeroPlacingTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PokerLeagueManager.Commands.Tests.Infrastructure;
+using PokerLeagueManager.Common.Commands;
+
+namespace PokerLeagueManager.Commands.Tests.EnterGameResults
+{
+    [TestClass]
+    public class ZeroPlacingTest : BaseTestFixture
+    {
+        private DateTime _gameDate = DateTime.Parse("03-Jul-1981");
+
+        [TestMethod]
+        public void ZeroPlacing()
+        {
+            var players = new List<EnterGameResultsCommand.GamePlayer>();
+            players.Add(new EnterGameResultsCommand.GamePlayer() { PlayerName = "Dylan Smith", Placing = 0, Winnings = 100 });
+
+            RunTest(new EnterGameResultsCommand() { GameDate = _gameDate, Players = players });
+        }
+
+        public override Exception ExpectedException()
+        {
+            return new ArgumentException();
+        }
+    }
+}
