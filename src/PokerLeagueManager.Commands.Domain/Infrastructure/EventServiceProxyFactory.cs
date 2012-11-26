f10b8f996ec2ccdc6b4649c01357940da270a9ec
Dylan Smith <dylansmith256@hotmail.com>
2012-11-26 01:51:06 +0000

    wrote some unit tests for EventServiceProxyFactory ***NO_CI***
    work-items: #65

--- src/PokerLeagueManager.Commands.Domain/Infrastructure/EventServiceProxyFactory.cs
+++ src/PokerLeagueManager.Commands.Domain/Infrastructure/EventServiceProxyFactory.cs
@@ -12,6 +12,11 @@ namespace PokerLeagueManager.Commands.Domain.Infrastructure
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Reliability", "CA2000:Dispose objects before losing scope", Justification = "EventServiceProxy doesn't have a Dispose method")]
         public IEventServiceProxy Create(DataRow row)
         {
+            if (row == null)
+            {
+                throw new ArgumentNullException("row");
+            }
+
             return new EventServiceProxy() { ServiceUrl = (string)row["SubscriberUrl"] };
         }
     }
