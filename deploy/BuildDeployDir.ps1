e073bb084ca5c13542f28f2bafb83caf6a436361
Dylan Smith <dylansmith256@hotmail.com>
2014-02-06 05:47:47 +0000

    Added a local deployment environment, and updated build environment to use different Web Site ***NO_CI***
    work-items: #103

--- deploy/BuildDeployDir.ps1
+++ deploy/BuildDeployDir.ps1
@@ -2,6 +2,30 @@ $build_outputs = "BuildOutputs"
 $deploy_dir = "Deployment"
 mkdir $deploy_dir
 
+### Local Environment ###
+
+$local_env_dir = "$deploy_dir\Local Environment"
+mkdir "$local_env_dir"
+
+$local_server_dir = "$local_env_dir\localhost"
+mkdir "$local_server_dir"
+copy -path "$build_outputs\Deploy\Deploy.ps1" -dest "$local_server_dir\Deploy.ps1"
+copy -path "$build_outputs\Deploy\Deploy.cmd" -dest "$local_server_dir\Deploy.cmd"
+
+mkdir "$local_server_dir\PokerLeagueManager.Commands.WCF"
+copy -path "$build_outputs\_PublishedWebsites\PokerLeagueManager.Commands.WCF_Package\*" -dest "$local_server_dir\PokerLeagueManager.Commands.WCF\"
+
+mkdir "$local_server_dir\PokerLeagueManager.Queries.WCF"
+copy -path "$build_outputs\_PublishedWebsites\PokerLeagueManager.Queries.WCF_Package\*" -dest "$local_server_dir\PokerLeagueManager.Queries.WCF\"
+
+mkdir "$local_server_dir\PokerLeagueManager.DB.EventStore"
+copy -path "$build_outputs\PokerLeagueManager.DB.EventStore.dacpac" -dest "$local_server_dir\PokerLeagueManager.DB.EventStore\"
+copy -path "$build_outputs\PokerLeagueManager.DB.EventStore.localhost.publish.xml" -dest "$local_server_dir\PokerLeagueManager.DB.EventStore\PokerLeagueManager.DB.EventStore.publish.xml"
+
+mkdir "$local_server_dir\PokerLeagueManager.DB.QueryStore"
+copy -path "$build_outputs\PokerLeagueManager.DB.QueryStore.dacpac" -dest "$local_server_dir\PokerLeagueManager.DB.QueryStore\"
+copy -path "$build_outputs\PokerLeagueManager.DB.QueryStore.localhost.publish.xml" -dest "$local_server_dir\PokerLeagueManager.DB.QueryStore\PokerLeagueManager.DB.QueryStore.publish.xml"
+
 ### Build Environment ###
 
 $build_env_dir = "$deploy_dir\Build Environment"
@@ -25,5 +49,7 @@ copy -path "$build_outputs\PokerLeagueManager.DB.EventStore.BUILD.publish.xml" -
 mkdir "$build_server_dir\PokerLeagueManager.DB.QueryStore"
 copy -path "$build_outputs\PokerLeagueManager.DB.QueryStore.dacpac" -dest "$build_server_dir\PokerLeagueManager.DB.QueryStore\"
 copy -path "$build_outputs\PokerLeagueManager.DB.QueryStore.BUILD.publish.xml" -dest "$build_server_dir\PokerLeagueManager.DB.QueryStore\PokerLeagueManager.DB.QueryStore.publish.xml"
+
+############################
 
 copy -path "$build_outputs\deploy\config\*" -dest "$deploy_dir\" -Force -Recurse
