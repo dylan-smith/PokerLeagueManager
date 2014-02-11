91eec6842f7f69c9435dacc84a9b59c3914a0ea7
Dylan Smith <dylansmith256@hotmail.com>
2014-02-11 03:46:54 +0000

    Added EventHandler for the new GetGamesList query ***NO_CI***
    work-items: #45

---
+++ src/PokerLeagueManager.Queries.Core/EventHandlers/GetGamesListHandler.cs
@@ -0,0 +1,34 @@
+﻿using PokerLeagueManager.Common.DTO;
+using PokerLeagueManager.Common.Events;
+using PokerLeagueManager.Queries.Core.Infrastructure;
+
+namespace PokerLeagueManager.Queries.Core.EventHandlers
+{
+    public class GetGamesListHandler : BaseHandler, IHandlesEvent<GameCreatedEvent>
+    {
+        public void Handle(GameCreatedEvent e)
+        {
+            QueryDataStore.Insert(new GetGamesListDto()
+            {
+                GameId = e.AggregateId,
+                GameDate = e.GameDate,
+                NumPlayers = 0,
+                Winner = string.Empty
+            });
+        }
+
+        public void Handle(PlayerAddedToGameEvent e)
+        {
+            var game = QueryDataStore.GetData<GetGamesListDto>(x => x.GameId == e.AggregateId);
+
+            game.NumPlayers++;
+            
+            if (e.Placing == 1)
+            {
+                game.Winner = e.PlayerName;
+            }
+            
+            QueryDataStore.Update<GetGamesListDto>(game);
+        }
+    }
+}
