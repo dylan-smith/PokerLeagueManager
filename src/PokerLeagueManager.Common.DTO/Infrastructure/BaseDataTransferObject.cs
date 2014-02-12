f81b25578673774504983e04061492dbb9a3e836
Dylan Smith <dylansmith256@hotmail.com>
2014-02-12 02:12:50 +0000

    Had to add DataContract attributes to base DTO class ***NO_CI***
    work-items: #44

--- src/PokerLeagueManager.Common.DTO/Infrastructure/BaseDataTransferObject.cs
+++ src/PokerLeagueManager.Common.DTO/Infrastructure/BaseDataTransferObject.cs
@@ -1,13 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PokerLeagueManager.Common.DTO.Infrastructure
 {
+    [DataContract]
     public class BaseDataTransferObject : IDataTransferObject
     {
+        public BaseDataTransferObject()
+        {
+            DtoId = Guid.NewGuid();
+        }
+
+        [DataMember]
         public Guid DtoId { get; set; }
     }
 }
