0e25f08b431c303baec60e48ec6cbc07fe62b3e2
Dylan Smith <dylansmith256@hotmail.com>
2014-02-15 22:33:30 +0000

    Included sample events in db project ***NO_CI***
    work-items: #124

--- src/PokerLeagueManager.DB.EventStore/Scripts/Script.PostDeployment.sql
+++ src/PokerLeagueManager.DB.EventStore/Scripts/Script.PostDeployment.sql
@@ -10,19 +10,5 @@ Post-Deployment Script Template
 --------------------------------------------------------------------------------------
 */
 
-DELETE FROM Subscribers
-
-IF '$(PublishEnvironment)' = 'dev'
-BEGIN
-INSERT INTO Subscribers(SubscriberId, SubscriberUrl) VALUES(newid(), 'http://localhost:1766/Infrastructure/EventService.svc')
-END
-
-IF '$(PublishEnvironment)' = 'Local'
-BEGIN
-INSERT INTO Subscribers(SubscriberId, SubscriberUrl) VALUES(newid(), 'http://localhost:83/PokerLeagueManager.Queries.WCF/Infrastructure/EventService.svc')
-END
-
-IF '$(PublishEnvironment)' = 'Build'
-BEGIN
-INSERT INTO Subscribers(SubscriberId, SubscriberUrl) VALUES(newid(), 'http://localhost:783/PokerLeagueManager.Queries.WCF/Infrastructure/EventService.svc')
-END
+:r .\Subscribers.sql
+:r .\SampleEvents.sql
