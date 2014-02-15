89ef0fcce855ce92605c6de352cfec53ed2e34e3
Dylan Smith <dylansmith256@hotmail.com>
2014-02-15 20:01:16 +0000

    Updated EventService URL's in sample data ***NO_CI***
    work-items: #108

--- src/PokerLeagueManager.DB.EventStore/Scripts/Script.PostDeployment.sql
+++ src/PokerLeagueManager.DB.EventStore/Scripts/Script.PostDeployment.sql
@@ -19,10 +19,10 @@ END
 
 IF '$(PublishEnvironment)' = 'Local'
 BEGIN
-INSERT INTO Subscribers(SubscriberId, SubscriberUrl) VALUES(newid(), 'http://localhost:83/PokerLeagueManager.Queries.WCF/EventService.svc')
+INSERT INTO Subscribers(SubscriberId, SubscriberUrl) VALUES(newid(), 'http://localhost:83/PokerLeagueManager.Queries.WCF/Infrastructure/EventService.svc')
 END
 
 IF '$(PublishEnvironment)' = 'Build'
 BEGIN
-INSERT INTO Subscribers(SubscriberId, SubscriberUrl) VALUES(newid(), 'http://localhost:783/PokerLeagueManager.Queries.WCF/EventService.svc')
+INSERT INTO Subscribers(SubscriberId, SubscriberUrl) VALUES(newid(), 'http://localhost:783/PokerLeagueManager.Queries.WCF/InfrastructureEventService.svc')
 END
