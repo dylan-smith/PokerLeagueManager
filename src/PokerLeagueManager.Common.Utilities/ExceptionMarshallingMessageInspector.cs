9213172ee5226048e7f3cb211e54cd0ac178dd20
Dylan Smith <dylansmith256@hotmail.com>
2014-03-15 22:33:16 +0000

    Added WCF Exception handling code to the Common project (not used yet) ***NO_CI***
    work-items: #128

---
+++ src/PokerLeagueManager.Common.Utilities/ExceptionMarshallingMessageInspector.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Description;
+using System.ServiceModel.Dispatcher;
+using System.Xml;
+
+namespace PokerLeagueManager.Common.Utilities
+{
+    public class ExceptionMarshallingMessageInspector : IClientMessageInspector
+    {
+        public void AfterReceiveReply(ref Message reply, object correlationState)
+        {
+            if (reply.IsFault)
+            {
+                // Create a copy of the original reply to allow default processing of the message
+                MessageBuffer buffer = reply.CreateBufferedCopy(int.MaxValue);
+                Message copy = buffer.CreateMessage();  // Create a copy to work with
+                reply = buffer.CreateMessage();         // Restore the original message
+
+                object faultDetail = ReadFaultDetail(copy);
+                Exception exception = faultDetail as Exception;
+                if (exception != null)
+                {
+                    throw exception;
+                }
+            }
+        }
+
+        public object BeforeSendRequest(ref Message request, IClientChannel channel)
+        {
+            return null;
+        }
+
+        private static object ReadFaultDetail(Message reply)
+        {
+            string detailElementName = "Detail";
+
+            using (XmlDictionaryReader reader = reply.GetReaderAtBodyContents())
+            {
+                // Find <soap:Detail>
+                while (reader.Read())
+                {
+                    if (reader.NodeType == XmlNodeType.Element && reader.LocalName == detailElementName)
+                    {
+                        break;
+                    }
+                }
+
+                // Did we find it?
+                if (reader.NodeType != XmlNodeType.Element || reader.LocalName != detailElementName)
+                {
+                    return null;
+                }
+
+                // Move to the contents of <soap:Detail>
+                if (!reader.Read())
+                {
+                    return null;
+                }
+
+                // Deserialize the fault
+                NetDataContractSerializer serializer = new NetDataContractSerializer();
+                try
+                {
+                    return serializer.ReadObject(reader);
+                }
+                catch (FileNotFoundException)
+                {
+                    // Serializer was unable to find assembly where exception is defined 
+                    return null;
+                }
+            }
+        }
+    }
+}
