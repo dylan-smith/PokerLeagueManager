c0f68cbd6964d7b7c38236078ea6898b932ac581
Dylan Smith <dylansmith256@hotmail.com>
2014-03-26 03:06:23 +0000

    Updated global exception handler to mark exceptions as handled to avoid crashing the UI ***NO_CI***
    work-items: #128

--- src/PokerLeagueManager.UI.WPF/MainWindow.xaml.cs
+++ src/PokerLeagueManager.UI.WPF/MainWindow.xaml.cs
@@ -26,6 +26,7 @@ namespace PokerLeagueManager.UI.Wpf
         private void GlobalExceptionHandler(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
         {
             MessageBox.Show(e.Exception.Message, "Unhandled Exception", MessageBoxButton.OK, MessageBoxImage.Error);
+            e.Handled = true;
             Application.Current.Shutdown();
         }
     }
