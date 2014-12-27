a0f0189bbbeb9e2aea73c321fe367504ddabe2f6
Dylan Smith <dylansmith256@hotmail.com>
2014-12-27 00:37:55 +0000

    Removed unnecessary CA suppression ***NO_CI***
    work-items: #249

--- src/PokerLeagueManager.UI.WPF/Views/IEnterGameResultsView.cs
+++ src/PokerLeagueManager.UI.WPF/Views/IEnterGameResultsView.cs
@@ -2,7 +2,6 @@
 
 namespace PokerLeagueManager.UI.Wpf.Views
 {
-    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1040:AvoidEmptyInterfaces", Justification = "Need an interface in order to make it mockable")]
     public interface IEnterGameResultsView
     {
         Guid GameId { get; set; }
