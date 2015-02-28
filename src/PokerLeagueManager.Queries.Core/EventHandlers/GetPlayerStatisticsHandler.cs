2dc05613da46fda0e90c09087c6416bdee6e4175
Dylan Smith <dylansmith256@hotmail.com>
2015-02-28 19:17:20 +0000

    Implemented GetPlayerStatistics event handler including a lookup query for it ***NO_CI***
    work-items: #290

---
+++ src/PokerLeagueManager.Queries.Core/EventHandlers/GetPlayerStatisticsHandler.cs
@@ -0,0 +1,59 @@
+﻿using System.Linq;
+using PokerLeagueManager.Common.DTO;
+using PokerLeagueManager.Common.DTO.DataTransferObjects.Lookups;
+using PokerLeagueManager.Common.Events;
+using PokerLeagueManager.Queries.Core.Infrastructure;
+
+namespace PokerLeagueManager.Queries.Core.EventHandlers
+{
+    public class GetPlayerStatisticsHandler : BaseHandler, IHandlesEvent<PlayerAddedToGameEvent>, IHandlesEvent<GameDeletedEvent>
+    {
+        public void Handle(PlayerAddedToGameEvent e)
+        {
+            var player = QueryDataStore.GetData<GetPlayerStatisticsDto>().FirstOrDefault(x => x.PlayerName == e.PlayerName);
+
+            if (player == null)
+            {
+                player = new GetPlayerStatisticsDto();
+
+                AddGameToPlayer(player, e);
+
+                QueryDataStore.Insert<GetPlayerStatisticsDto>(player);
+            }
+            else
+            {
+                AddGameToPlayer(player, e);
+
+                QueryDataStore.SaveChanges();
+            }
+        }
+
+        public void Handle(GameDeletedEvent e)
+        {
+            var players = QueryDataStore.GetData<LookupGamePlayersDto>().Where(x => x.GameId == e.AggregateId);
+
+            foreach (var p in players)
+            {
+                var stats = QueryDataStore.GetData<GetPlayerStatisticsDto>().First(x => x.PlayerName == p.PlayerName);
+
+                stats.GamesPlayed--;
+                stats.Winnings -= p.Winnings;
+                stats.PayIn -= p.PayIn;
+                stats.Profit -= p.Winnings - p.PayIn;
+                stats.ProfitPerGame = stats.Profit == 0 ? 0 : stats.Profit / stats.GamesPlayed;
+
+                QueryDataStore.SaveChanges();
+            }
+        }
+
+        private void AddGameToPlayer(GetPlayerStatisticsDto player, PlayerAddedToGameEvent e)
+        {
+            player.PlayerName = e.PlayerName;
+            player.GamesPlayed++;
+            player.Winnings += e.Winnings;
+            player.PayIn += e.PayIn;
+            player.Profit += e.Winnings - e.PayIn;
+            player.ProfitPerGame = player.Profit / player.GamesPlayed;
+        }
+    }
+}
---
+++ src/PokerLeagueManager.Queries.Core/EventHandlers/LookupGamePlayersHandler.cs
@@ -0,0 +1,21 @@
+﻿using PokerLeagueManager.Common.DTO.DataTransferObjects.Lookups;
+using PokerLeagueManager.Common.Events;
+using PokerLeagueManager.Queries.Core.Infrastructure;
+
+namespace PokerLeagueManager.Queries.Core.EventHandlers
+{
+    public class LookupGamePlayersHandler : BaseHandler, IHandlesEvent<PlayerAddedToGameEvent>
+    {
+        public void Handle(PlayerAddedToGameEvent e)
+        {
+            var dto = new LookupGamePlayersDto();
+
+            dto.GameId = e.AggregateId;
+            dto.Winnings = e.Winnings;
+            dto.PayIn = e.PayIn;
+            dto.PlayerName = e.PlayerName;
+
+            QueryDataStore.Insert<LookupGamePlayersDto>(dto);
+        }
+    }
+}
