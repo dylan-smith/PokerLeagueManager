b9d39f0a80ce082a7262f523dc382ae7ec8efe63
Dylan Smith <dylansmith256@hotmail.com>
2015-03-02 08:24:09 +0000

    Implemented GetGamesWithPlayers event handler and tests ***NO_CI***
    work-items: #310

---
+++ src/PokerLeagueManager.Queries.Core/EventHandlers/GetGamesWithPlayerHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using PokerLeagueManager.Common.DTO;
+using PokerLeagueManager.Common.Events;
+using PokerLeagueManager.Queries.Core.Infrastructure;
+
+namespace PokerLeagueManager.Queries.Core.EventHandlers
+{
+    public class GetGamesWithPlayerHandler : BaseHandler, IHandlesEvent<PlayerAddedToGameEvent>, IHandlesEvent<GameDeletedEvent>
+    {
+        public void Handle(PlayerAddedToGameEvent e)
+        {
+            var dto = new GetGamesWithPlayerDto();
+            dto.GameId = e.AggregateId;
+            dto.PlayerName = e.PlayerName;
+
+            QueryDataStore.Insert<GetGamesWithPlayerDto>(dto);
+        }
+
+        public void Handle(GameDeletedEvent e)
+        {
+            var dtos = QueryDataStore.GetData<GetGamesWithPlayerDto>().Where(x => x.GameId == e.AggregateId).ToList();
+
+            foreach (var d in dtos)
+            {
+                QueryDataStore.Delete<GetGamesWithPlayerDto>(d);
+            }
+        }
+    }
+}
