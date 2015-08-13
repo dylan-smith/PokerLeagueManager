52e7d828f724746a813ea65d5075fb2b13587c6e
Dylan Smith <dylansmith256@hotmail.com>
2015-08-13 00:48:26 +0000

    Added template for AzureSimple ***NO_CI***
    work-items: #332

---
+++ deploy/Azure ARM/AzureSimple/DeployAzure.ps1
@@ -0,0 +1,21 @@
+﻿#Requires -Version 3.0
+
+Param(
+  [string] [Parameter(Mandatory=$true)]$ResourceGroupName
+)
+
+Set-StrictMode -Version 3
+Import-Module Azure -ErrorAction SilentlyContinue
+
+Add-AzureAccount -verbose
+Select-AzureSubscription -SubscriptionName "MSDN MPN"
+
+$ResourceGroupLocation = "West US"
+$TemplateFile = Join-Path $PSScriptRoot "AzureSimple.json"
+
+# Create or update the resource group using the specified template file and template parameters file
+Switch-AzureMode AzureResourceManager
+New-AzureResourceGroup -Name $ResourceGroupName `
+                       -Location $ResourceGroupLocation `
+                       -TemplateFile $TemplateFile `
+                       -Force -Verbose
