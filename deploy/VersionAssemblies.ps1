9fc06ad9521d6a04b5d5fa68b6060d38611d331d
Dylan Smith <dylansmith256@hotmail.com>
2016-09-06 19:35:55 +0000

    added script to version assemblies in build ***NO_CI***
    work-items: #165, #8133, #8134, #8135

---
+++ deploy/VersionAssemblies.ps1
@@ -0,0 +1,46 @@
+﻿[CmdletBinding()]
+param(
+    [string]$MajorVersion,
+    [string]$MinorVersion,
+    [string]$BuildNumber,
+    [string]$SourcesDir
+)
+
+Write-Output "MajorVersion: $MajorVersion"
+Write-Output "MinorVersion: $MinorVersion"
+Write-Output "BuildNumber: $BuildNumber"
+Write-Output "SourcesDir: $SourcesDir"
+
+$Year = Get-Date -Format yy
+$Month = Get-Date -Format MM
+$Day = Get-Date -Format dd
+
+$DotPosition = $BuildNumber.IndexOf(".")
+$Revision = $BuildNumber.Substring($DotPosition + 1)
+$Revision = $Revision.PadLeft(3, "0")
+
+$NewVersion = "$MajorVersion.$MinorVersion.$Year$Month.$Day$Revision"
+
+Write-Output "New Version: $NewVersion"
+
+$VersionRegex = "\d+\.\d+\.\d+\.\d+"
+
+# Apply the version to the assembly property files
+$files = gci $SourcesDir -recurse -include "*Properties*","My Project" | 
+	?{ $_.PSIsContainer } | 
+	foreach { gci -Path $_.FullName -Recurse -include AssemblyInfo.* }
+if($files)
+{
+	Write-Verbose "Will apply $NewVersion to $($files.count) files."
+	
+	foreach ($file in $files) {
+		$filecontent = Get-Content($file)
+		attrib $file -r
+		$filecontent -replace $VersionRegex, $NewVersion | Out-File -Encoding "UTF8" $file
+		Write-Verbose "$file - version applied"
+	}
+}
+else
+{
+	Write-Warning "Found no files."
+}
