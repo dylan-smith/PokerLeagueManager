b8bb220166def1d0eba645a383b6bcdf748b9ae9
Dylan Smith <dylansmith256@hotmail.com>
2016-09-07 16:44:27 +0000

    deleted old test framework project folder ***NO_CI***
    work-items: #165, #274, #8121, #8124, #8125, #8133, #8134, #8135

--- src/PokerLeagueManager.UI.Wpf.TestFramework/GlobalSuppressions.cs
+++
@@ -1,9 +0,0 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
-
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.OrderingRules", "SA1201:Elements must appear in the correct order", Justification = "Private properties should come at the end in test framework", Scope = "type", Target = "PokerLeagueManager.UI.Wpf.TestFramework.GameResultsScreen")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.OrderingRules", "SA1201:Elements must appear in the correct order", Justification = "Private properties should come at the end in test framework", Scope = "type", Target = "PokerLeagueManager.UI.Wpf.TestFramework.GamesListScreen")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.OrderingRules", "SA1201:Elements must appear in the correct order", Justification = "Private properties should come at the end in test framework", Scope = "type", Target = "PokerLeagueManager.UI.Wpf.TestFramework.EnterGameResultsScreen")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.OrderingRules", "SA1201:Elements must appear in the correct order", Justification = "Private properties should come at the end in test framework", Scope = "type", Target = "PokerLeagueManager.UI.Wpf.TestFramework.PlayerDetailsScreen")]
