45ea9f4915fbda5cad79770df29442af9c8274e9
Dylan Smith <dylansmith256@hotmail.com>
2016-09-12 10:40:11 -0500

    Removed SQLCMD var from DB project
    Related Work Items: #8122

--- src/PokerLeagueManager.DB.EventStore/Scripts/Subscribers.sql
+++ src/PokerLeagueManager.DB.EventStore/Scripts/Subscribers.sql
@@ -1,26 +1,7 @@
 ﻿DELETE FROM Subscribers
 
-IF '$(PublishEnvironment)' = 'dev'
-BEGIN
 INSERT INTO Subscribers(SubscriberId, SubscriberUrl) VALUES(newid(), 'http://localhost:1766/Infrastructure/EventService.svc')
-END
-
-IF '$(PublishEnvironment)' = 'Local'
-BEGIN
 INSERT INTO Subscribers(SubscriberId, SubscriberUrl) VALUES(newid(), 'http://localhost:1766/Infrastructure/EventService.svc')
-END
-
-IF '$(PublishEnvironment)' = 'Build'
-BEGIN
 INSERT INTO Subscribers(SubscriberId, SubscriberUrl) VALUES(newid(), 'http://localhost:783/PokerLeagueManager.Queries.WCF/Infrastructure/EventService.svc')
-END
-
-IF '$(PublishEnvironment)' = 'AzureSimple'
-BEGIN
 INSERT INTO Subscribers(SubscriberId, SubscriberUrl) VALUES(newid(), 'http://localhost/PokerLeagueManager.Queries.WCF/Infrastructure/EventService.svc')
-END
-
-IF '$(PublishEnvironment)' = 'AzureComplex'
-BEGIN
 INSERT INTO Subscribers(SubscriberId, SubscriberUrl) VALUES(newid(), 'http://QueryWeb/PokerLeagueManager.Queries.WCF/Infrastructure/EventService.svc')
-END
