966b8b892c900be1f65c4626b88892d626593b7f
Dylan Smith <dylansmith256@hotmail.com>
2016-09-12 17:05:10 -0500

    Added batch file to run CUIT's easier
    Related Work Items: #319

---
+++ src/RunCUIT.bat
@@ -0,0 +1,3 @@
+"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" "PokerLeagueManager.UI.Wpf.CodedUITests\bin\Debug\PokerLeagueManager.UI.Wpf.CodedUITests.dll"
+
+pause
