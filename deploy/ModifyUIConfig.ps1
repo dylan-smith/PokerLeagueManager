f4f44d1983d23d492204ae635d1fa1c7a55bbf7c
Dylan Smith <dylansmith256@hotmail.com>
2016-09-20 22:26:00 -0500

    moved command service to appsettings
    Related Work Items: #8143

---
+++ deploy/ModifyUIConfig.ps1
@@ -0,0 +1,15 @@
+﻿param (
+    [string]$queryServiceUrl,
+    [string]$commandServiceUrl,
+    [string]$configPath
+)
+
+$config = [xml](Get-Content $configPath)
+
+$querySetting = $config.configuration.appSettings.add | where {$_.Key -eq 'QueryServiceUrl'}
+$commandSetting = $config.configuration.appSettings.add | where {$_.Key -eq 'CommandServiceUrl'}
+
+$querySetting.value = $queryServiceUrl
+$commandSetting.value = $commandServiceUrl
+
+$config.Save($configPath)
--- src/PokerLeagueManager.Common/Infrastructure/CommandServiceProxy.cs
+++ src/PokerLeagueManager.Common/Infrastructure/CommandServiceProxy.cs
@@ -1,9 +1,16 @@
-﻿using System.ServiceModel;
+﻿using System.Configuration;
+using System.ServiceModel;
 
 namespace PokerLeagueManager.Common.Infrastructure
 {
     public class CommandServiceProxy : ClientBase<ICommandService>, ICommandService
     {
+        public CommandServiceProxy()
+        {
+            var commandUrl = ConfigurationManager.AppSettings["CommandServiceUrl"];
+            base.Endpoint.Address = new EndpointAddress(commandUrl);
+        }
+
         public void ExecuteCommand(ICommand command)
         {
             base.Channel.ExecuteCommand(command);
