6373143ffcb870b26ecff1426138c959dfa1de5f
Dylan Smith <dylansmith256@hotmail.com>
2016-09-22 13:38:10 -0500

    Updated devdeploy.bat to generate sample data
    Related Work Items: #272

--- src/DevDeploy.bat
+++ src/DevDeploy.bat
@@ -3,8 +3,7 @@ del C:\PokerLeagueManager.UI.Wpf\* /S /Q
 xcopy PokerLeagueManager.UI.Wpf\bin\Debug\* C:\PokerLeagueManager.UI.Wpf\
 "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\Extensions\Microsoft\SQLDB\DAC\130\sqlpackage.exe" /SourceFile:"PokerLeagueManager.DB.EventStore\bin\Debug\PokerLeagueManager.DB.EventStore.dacpac" /Action:Publish /TargetServerName:localhost /TargetDatabaseName:PokerLeagueManager.DB.EventStore /p:CreateNewDatabase=true
 "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\Extensions\Microsoft\SQLDB\DAC\130\sqlpackage.exe" /SourceFile:"PokerLeagueManager.DB.QueryStore\bin\Debug\PokerLeagueManager.DB.QueryStore.dacpac" /Action:Publish /TargetServerName:localhost /TargetDatabaseName:PokerLeagueManager.DB.QueryStore /p:CreateNewDatabase=true
-PokerLeagueManager.Utilities.CreateEventSubscriber\bin\Debug\PokerLeagueManager.Utilities.CreateEventSubscriber.exe localhost PokerLeagueManager.DB.EventStore http://localhost:1766/EventService.svc
-
-REM "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" "PokerLeagueManager.UI.Wpf.CodedUITests\bin\Debug\PokerLeagueManager.UI.Wpf.CodedUITests.dll"
+PokerLeagueManager.Utilities\bin\Debug\PokerLeagueManager.Utilities.exe CreateEventSubscriber localhost PokerLeagueManager.DB.EventStore http://localhost:1766/EventService.svc
+PokerLeagueManager.Utilities\bin\Debug\PokerLeagueManager.Utilities.exe GenerateSampleData http://localhost:1761/CommandService.svc
 
 pause
