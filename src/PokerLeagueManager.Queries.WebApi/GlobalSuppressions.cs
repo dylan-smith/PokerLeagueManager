f4155f85722e9bd345382b30d24b924e8eb5b61c
Dylan Smith <dylansmith256@hotmail.com>
2016-09-29 12:08:48 -0500

    fixing build error by adding suppressmessage
    Related Work Items: #8163

---
+++ src/PokerLeagueManager.Queries.WebApi/GlobalSuppressions.cs
@@ -0,0 +1,4 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1649:File name must match first type name", Justification = "Default name used in ASPNET template", Scope = "type", Target = "~T:PokerLeagueManager.Queries.WebApi.WebApiApplication")]
+[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1649:File name must match first type name", Justification = "Default name used in ASPNET template", Scope = "namespace", Target = "PokerLeagueManager.Queries.WebApi")]
