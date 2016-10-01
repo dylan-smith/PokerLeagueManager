57f6fa2ab31504d3d9f03f3edeadb32b95d3d821
Dylan Smith <dylansmith256@hotmail.com>
2016-09-30 19:39:56 -0500

    Added global nav menu items
    Related Work Items: #8162

--- src/PokerLeagueManager.UI.Web/components/pokerApp/pokerApp.component.js
+++ src/PokerLeagueManager.UI.Web/components/pokerApp/pokerApp.component.js
@@ -7,6 +7,12 @@
         templateUrl: '/components/pokerApp/pokerApp.component.html',
         $routeConfig: [
             { path: '/', component: 'gameList', name: 'Home' },
+            { path: '/Games', component: 'gameList', name: 'Games' },
+            { path: '/Stats', component: 'playerStats', name: 'Stats' },
+            { path: '/POTY', component: 'playerOfTheYear', name: 'POTY' },
+            { path: '/Videos', component: 'videos', name: 'Videos' },
+            { path: '/Pictures', component: 'pictures', name: 'Pictures' },
+            { path: '/Rules', component: 'rules', name: 'Rules' },
             { path: '/**', redirectTo: ['Home', ''] }
         ]
     });
