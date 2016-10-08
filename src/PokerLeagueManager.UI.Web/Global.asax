e7189296d6b97c5e0a953fd80b4da0d7d9d7b354
Dylan Smith <dylansmith256@hotmail.com>
2016-10-08 16:52:38 -0500

    Added AppInsights to UI.Web and updated index.html to be a razor page with AI telemetry

---
+++ src/PokerLeagueManager.UI.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PokerLeagueManager.UI.Web.MvcApplication" Language="C#" %>
---
+++ src/PokerLeagueManager.UI.Web/Global.asax.cs
@@ -0,0 +1,17 @@
+﻿using System.Configuration;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Microsoft.ApplicationInsights.Extensibility;
+
+namespace PokerLeagueManager.UI.Web
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            TelemetryConfiguration.Active.InstrumentationKey = ConfigurationManager.AppSettings["AppInsightsKey"];
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
