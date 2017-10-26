f8cb62c50f7b9fca96f832e996f4e0bc17365327
Dylan Smith <dylansmith256@hotmail.com>
2017-10-25 19:55:52 -0500

    almost got sidenav looking good

--- spikes/Material2/src/app/app.module.ts
+++ spikes/Material2/src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { MatButtonModule, MatExpansionModule, MatSidenavModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { GameListComponent } from './game-list/game-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatSidenavModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot([
+      { path: '', component: GameListComponent},
+      { path: 'Home', component: GameListComponent},
+      { path: 'Games', component: GameListComponent },
+      { path: 'Stats', component: GameListComponent }, 
+      { path: 'POTY', component: GameListComponent }, 
+      { path: 'Media', component: GameListComponent }, 
+      { path: 'Rules', component: GameListComponent }, 
+      { path: '**', component: GameListComponent }
+  ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
