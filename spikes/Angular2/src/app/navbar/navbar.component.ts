87dbef02be267af07ca44b55c4f11d893537ee4e
Dylan Smith <dylansmith256@hotmail.com>
2017-10-26 13:44:02 -0500

    burger button shows/hides based on screen size

--- spikes/Angular2/src/app/navbar/navbar.component.ts
+++ spikes/Angular2/src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import {Component, NgModule, EventEmitter, Output} from '@angular/core';
+import {Component, NgModule, EventEmitter, Output, NgZone, OnInit} from '@angular/core';
 import {MatButtonModule, MatMenuModule} from '@angular/material';
 import {CommonModule} from '@angular/common';
 import {RouterModule} from '@angular/router';
@@ -8,10 +8,23 @@ import {RouterModule} from '@angular/router';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent { 
+export class NavbarComponent implements OnInit{ 
+  private mediaMatcher: MediaQueryList = matchMedia(`(max-width: 840px)`);
+
   @Output() SideNavToggled = new EventEmitter<void>();
+
+  constructor(zone: NgZone) { 
+    this.mediaMatcher.addListener(mql => zone.run(() => this.mediaMatcher = mql));
+  }
+
+  ngOnInit(): void {
+  }
 
   toggleSideNav() {
     this.SideNavToggled.emit();
+  }
+
+  isScreenSmall(): boolean {
+    return this.mediaMatcher.matches;
   }
 }
