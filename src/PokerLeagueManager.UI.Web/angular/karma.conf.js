3a9d113306022a22c0d2d28d36b49d04806480b4
Dylan Smith <dylansmith256@hotmail.com>
2017-11-16 17:23:16 -0600

    added phantomjs and junit reporter

--- src/PokerLeagueManager.UI.Web/angular/karma.conf.js
+++ src/PokerLeagueManager.UI.Web/angular/karma.conf.js
@@ -10,7 +10,9 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular/cli/plugins/karma')
+      require('@angular/cli/plugins/karma'),
+      require('karma-phantomjs-launcher'),
+      require('karma-junit-reporter')
     ],
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
@@ -22,12 +24,16 @@ module.exports = function (config) {
     angularCli: {
       environment: 'dev'
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress', 'kjhtml', 'junit'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false
+    browsers: ['PhatomJS'],
+    singleRun: false,
+    junitReporter: {
+      outputDir: '',
+      outputFile: 'test.xml'
+    }
   });
 };
