2e5e3a9214e3ce82683b266801012d6d178266cb
Dylan Smith <dylansmith256@hotmail.com>
2017-11-16 21:29:52 -0600

    added testing to npm commands and batch file

--- src/PokerLeagueManager.UI.Web/BuildAngular.bat
+++ src/PokerLeagueManager.UI.Web/BuildAngular.bat
@@ -2,5 +2,6 @@ call %~dp0\GenerateTypeScript.bat
 cd %~dp0\angular
 call npm install
 call npm run build-prod
+call npm run test-prod
 cd ..
 powershell %~dp0\..\..\deploy\TransformIndex.ps1 -SourcePath %~dp0\angular\dist\index.html -TargetPath %~dp0\angular\dist\index.cshtml
