604fd6907aa940a25e8e9ebf0ff1013a0a038916
Dylan Smith <dylansmith256@hotmail.com>
2017-11-18 20:37:00 -0600

    remove ng test from BuildAngular.bat

--- src/PokerLeagueManager.UI.Web/BuildAngular.bat
+++ src/PokerLeagueManager.UI.Web/BuildAngular.bat
@@ -8,9 +8,6 @@ if not "%errorlevel%"=="0" exit /b 1
 call npm run build-prod
 if not "%errorlevel%"=="0" exit /b 1
 
-call npm run test-prod
-if not "%errorlevel%"=="0" exit /b 1
-
 cd ..
 powershell %~dp0\..\..\deploy\TransformIndex.ps1 -SourcePath %~dp0\angular\dist\index.html -TargetPath %~dp0\angular\dist\index.cshtml
 if not "%errorlevel%"=="0" exit /b 1
