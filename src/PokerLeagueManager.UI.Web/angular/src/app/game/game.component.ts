f61931249bcb5903f8b4c0a6b597d6baf5f3476b
Dylan Smith <dylansmith256@hotmail.com>
2017-11-18 18:39:49 -0600

    intentionally breaking the typescript to make sure the VSTS buid fails

--- src/PokerLeagueManager.UI.Web/angular/src/app/game/game.component.ts
+++ src/PokerLeagueManager.UI.Web/angular/src/app/game/game.component.ts
@@ -17,7 +17,7 @@ export class GameComponent implements OnInit {
   displayedColumns = ['Placing', 'PlayerName', 'Winnings', 'PayIn'];
   private mediaMatcher: MediaQueryList = matchMedia(`(max-width: 530px)`);
 
-  constructor(private queryService: QueryService, private appInsightsService: AppInsightsService, zone: NgZone) {
+  constrxxxuctor(private queryService: QueryService, private appInsightsService: AppInsightsService, zone: NgZone) {
     this.mediaMatcher.addListener(mql => zone.run(() => this.mediaMatcher = mql));
   }
 
