f541efddeebd8540013a7bddb8640b53659bbeb8
Dylan Smith <dylansmith256@hotmail.com>
2017-11-18 19:34:20 -0600

    added error checking to BuildAngular.bat

--- src/PokerLeagueManager.UI.Web/BuildAngular.bat
+++ src/PokerLeagueManager.UI.Web/BuildAngular.bat
@@ -1,7 +1,16 @@
 call %~dp0\GenerateTypeScript.bat
+if not "%errorlevel%"=="0" exit /b 1
+
 cd %~dp0\angular
 call npm install
+if not "%errorlevel%"=="0" exit /b 1
+
 call npm run build-prod
+if not "%errorlevel%"=="0" exit /b 1
+
 call npm run test-prod
+if not "%errorlevel%"=="0" exit /b 1
+
 cd ..
 powershell %~dp0\..\..\deploy\TransformIndex.ps1 -SourcePath %~dp0\angular\dist\index.html -TargetPath %~dp0\angular\dist\index.cshtml
+if not "%errorlevel%"=="0" exit /b 1
