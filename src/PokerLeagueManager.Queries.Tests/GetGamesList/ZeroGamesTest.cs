5633cb5ff02cc13da002d08c577c707765644943
Dylan Smith <dysmith@microsoft.com>
2019-09-24 10:08:25 +0200

    Added test for Zero games - GetGamesList

---
+++ src/PokerLeagueManager.Queries.Tests/GetGamesList/ZeroGamesTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PokerLeagueManager.Common.Queries;
+using PokerLeagueManager.Queries.Tests.Infrastructure;
+
+namespace PokerLeagueManager.Queries.Tests.GetGamesList
+{
+    [TestClass]
+    public class ZeroGamesTest : BaseQueryTest
+    {
+        [TestMethod]
+        public void GetGamesList_TwoGames()
+        {
+            var result = SetupQueryService().Execute(new GetGamesListQuery());
+
+            Assert.AreEqual(0, result.Count());
+        }
+    }
+}
